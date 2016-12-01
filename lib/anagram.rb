40ecd82fc60fdf8630654d6188c39fb340bc7cc7
github username <github email address>
2016-12-01 14:42:36 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+
+      @word = word.split("").sort.join
+
+  end
+
+  def match(array)
+    n=0
+    final_array = []
+
+    while n < array.length
+      if @word == array[n].split("").sort.join
+        final_array << array[n]
+      end
+        n+=1
+    
+    end
+    return final_array
+  end
+
+
+  #def match
+
+
+end
